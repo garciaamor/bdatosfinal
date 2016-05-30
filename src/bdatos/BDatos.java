e7f448439aa79bf39105bc185d700ad744a466da
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-05-30 09:17:12 +0200

    BaseDatos

--- src/bdatos/BDatos.java
+++ src/bdatos/BDatos.java
@@ -9,8 +9,7 @@ public class BDatos {
 
     public static void main(String[] args) {
         
-        /*ConexionMySQL obx = new ConexionMySQL();
-        obx.Conectar();*/
+        
         
         FrameDatos fram = new FrameDatos();
         fram.setVisible(true);
